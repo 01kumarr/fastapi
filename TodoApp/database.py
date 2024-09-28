605702ba84218d0fa2b757beaeb1ab24fc6e969f
Vivek Kumar <vkzzz@outlook.com>
2024-09-28 22:44:56 +0530

    adding init file and creating database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,1 +1,8 @@
+from sqlalchemy import create_engine
+
+## database location in sqlite..with name (todos)
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db' 
+
+# create an engine to communicate with database
+enigne = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) 
+
