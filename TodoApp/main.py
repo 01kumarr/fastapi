c984025bc45f21c22ace997879b3a765240dd511
Vivek Kumar <vkzzz@outlook.com>
2024-09-29 11:09:40 +0530

    data insertion into db

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI 
-import models 
-from database import engine 
+from typing import Annotated
+from sqlalchemy.orm import Session 
+from fastapi import FastAPI, Depends
+import models
+from models import Todos
+from database import engine, SessionLocal
 
 app = FastAPI() 
 
 # create a table in the database todos
 models.Base.metadata.create_all(bind=engine) 
+
+
+## create a functimn to get database 
+def get_db():
+    db = SessionLocal() 
+    try:
+        yield db 
+    finally:
+        db.close() 
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get('/')
+async def read_db(db: db_dependency):
+    return db.query(Todos).all()
+
+
