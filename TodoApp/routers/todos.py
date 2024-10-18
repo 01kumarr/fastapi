d4143e4e36dd3447f3f34e99bc04d1aaa300ab7a
1123vivek <vivek.kumar@greenridertech.com>
2024-10-18 17:10:36 +0530

    Authentication method added to extract all data of logged user

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -1,10 +1,11 @@
 from typing import Annotated
 from sqlalchemy.orm import Session 
 from fastapi import APIRouter, Depends, HTTPException, Path
-from models import Todos
+from models import Todos, Users
 from database import SessionLocal
 from starlette import status
 from pydantic import BaseModel, Field 
+from .auth import get_current_user
 
 router = APIRouter() 
 
@@ -18,6 +19,7 @@ def get_db():
         db.close() 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependancy = Annotated[dict, Depends(get_current_user)]
 
 
 class TodoRequest(BaseModel):
@@ -28,8 +30,8 @@ class TodoRequest(BaseModel):
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def read_db(db: db_dependency):
-    return db.query(Todos).all()
+async def read_db(user : user_dependancy, db: db_dependency):
+    return db.query(Todos).filter(Todos.owner_id == user.get('id')).all()
 
 
 @router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
@@ -42,8 +44,12 @@ async def read_todo(db: db_dependency, todo_id : int = Path(gt=0)):
 
 
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(db : db_dependency, todo_request : TodoRequest):
-    todo_model = Todos(**todo_request.model_dump()) 
+async def create_todo(user : user_dependancy, db : db_dependency, 
+                      todo_request : TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed!")
+
+    todo_model = Todos(**todo_request.model_dump(), owner_id = user.get('id')) 
 
     db.add(todo_model) 
     db.commit() 
